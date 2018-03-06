91895958cb43357461758747881f232f6518b87b
lcavasso <35540322+lcavasso@users.noreply.github.com>
2018-03-06 11:58:52 -0800

    Add files via upload

--- scripts/appraisal_analysis.R
+++ scripts/appraisal_analysis.R
@@ -1,7 +1,8 @@
 # code is still in development
 
 # Read file
-# annotations <- read_csv("~/_folders I made myself/Research/Discourse Processing/socc_comments/combined_appraisal_comments.csv")
+# library(readr)
+# annotations <- read_csv("~/_folders I made myself/Research/Discourse Processing/socc_comments/corrected_combined_appraisal_comments.csv")
 
 # remove the extra column
 annotations <- annotations[,2:11]
@@ -71,3 +72,74 @@ gracounts$focus = c(
   length(focusrows[focusrows$grapol=="up",]$gralab),
   length(focusrows[focusrows$grapol=="down",]$gralab)
 )
+
+# Check for any blank annotations
+mismatches = subset(annotations,
+                    annotations$attlab == "*" | annotations$attpol == "*" | annotations$gralab == "*" | annotations$grapol == "*")
+
+# find stacked spans
+stacks = annotations[0,]
+look = 10     # how far ahead to look
+punctuation = c(".", ",", "!", "?")
+for(i in 1:length(annotations$span))
+{
+  for(n in 1:look)
+  {
+    if (match(i+n, 1:length(annotations$span), nomatch=FALSE))
+    {
+      if(annotations$charstart[i+n] <= annotations$charend[i]  # if the charstart for the 'next' is less than charstart for this one
+         & annotations$comment[i+n] == annotations$comment[i]  # and the two are in the same comment
+         & !(annotations$span[i] %in% punctuation)              # and this span is not just punctuation
+         & !(annotations$span[i+n] %in% punctuation))           # and neither is the 'next' one
+      {
+        stacks = rbind(annotations[i,], stacks)  # then add this row to stacks
+        stacks = rbind(annotations[i+n,], stacks) # and the 'next' one
+      }
+    }
+  }
+}
+stacks = unique(stacks)
+stacks = stacks[order(stacks[,1], stacks[,4]),]
+
+# get a data.frame of all spans with graduation and the attitude spans that contain them
+graduation.all = stacks[0,]
+look = 10     # how far ahead to look
+for(i in 1:length(stacks$span))
+{
+  for(n in 1:look)
+  {
+    # look ahead, if we see a grad annotation for this comment and an attitude one for an overlapping future one then we can include them
+    if (match(i+n, 1:length(stacks$span), nomatch=FALSE))
+    {
+      if(stacks$charstart[i+n] <= stacks$charend[i]  # if the charstart for the 'next' is less than charend for this one
+         & stacks$comment[i+n] == stacks$comment[i]  # and the two are in the same comment
+         & !(stacks$span[i] %in% punctuation)        # and this span is not just punctuation
+         & !(stacks$span[i+n] %in% punctuation)      # and neither is the 'next' one
+         & (stacks$gralab[i] != "None" | stacks$grapol[i] != "None")  # and there's a graduation annotation in this row
+         & (stacks$attlab[i+n] != "None" | stacks$attpol[i+n] != "None") # and an attitude annotation in the 'next' one
+         )
+      {
+        graduation.all = rbind(stacks[i,], graduation.all)  # then add this row to the df (graduation annotation)
+        graduation.all = rbind(stacks[i+n,], graduation.all) # and the 'next' one (attitude annotation)
+      }
+    }
+    # look behind for the same thing
+    if (match(i-n, 1:length(stacks$span), nomatch=FALSE))
+    {
+      if(stacks$charstart[i] <= stacks$charend[i-n]  # if the charstart for this one is greater than charend for the 'previous' one
+         & stacks$comment[i-n] == stacks$comment[i]  # and the two are in the same comment
+         & !(stacks$span[i] %in% punctuation)        # and this span is not just punctuation
+         & !(stacks$span[i-n] %in% punctuation)      # and neither is the 'next' one
+         & (stacks$gralab[i] != "None" | stacks$grapol[i] != "None")  # and there's a graduation annotation in this row
+         & (stacks$attlab[i-n] != "None" | stacks$attpol[i-n] != "None") # and an attitude annotation in the 'previous' one
+      )
+      {
+        graduation.all = rbind(stacks[i,], graduation.all)  # then add this row to the df (graduation annotation)
+        graduation.all = rbind(stacks[i-n,], graduation.all) # and the 'previous' one (attitude annotation)
+      }
+    }
+  }
+}
+
+graduation.all = unique(graduation.all)
+graduation.all = graduation.all[order(graduation.all[,1], graduation.all[,4]),]
