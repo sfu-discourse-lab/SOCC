16bfd47d274f9e66764f3a950df95eb5e654df75
lcavasso <35540322+lcavasso@users.noreply.github.com>
2018-03-06 14:46:12 -0800

    Add files via upload

--- scripts/appraisal_analysis.R
+++ scripts/appraisal_analysis.R
@@ -1,5 +1,6 @@
 # code is still in development
 
+#### Getting the annotation dataframe ####
 # Read file
 # library(readr)
 # annotations <- read_csv("~/_folders I made myself/Research/Discourse Processing/socc_comments/corrected_combined_appraisal_comments.csv")
@@ -7,6 +8,11 @@
 # remove the extra column
 annotations <- annotations[,2:11]
 
+# Check for any blank annotations
+mismatches = subset(annotations,
+                    annotations$attlab == "*" | annotations$attpol == "*" | annotations$gralab == "*" | annotations$grapol == "*")
+
+#### Getting basic counts ####
 # So that we can count them later, get subsets of "annotations" by label
 # Attitude labels
 approws = annotations[annotations$attlab=="Appreciation",]
@@ -23,7 +29,7 @@ focusrows = annotations[annotations$gralab=="Focus",]
 upgradrows = annotations[annotations$grapol=="up",]$grapol
 downgradrows = annotations[annotations$grapol=="down",]
 
-# Get simple counts
+# get simple counts
 # Attitude labels
 appcount = length(approws$attlab)
 judcount = length(judrows$attlab)
@@ -73,9 +79,7 @@ gracounts$focus = c(
   length(focusrows[focusrows$grapol=="down",]$gralab)
 )
 
-# Check for any blank annotations
-mismatches = subset(annotations,
-                    annotations$attlab == "*" | annotations$attpol == "*" | annotations$gralab == "*" | annotations$grapol == "*")
+#### Finding stacked spans and graduation ####
 
 # find stacked spans
 stacks = annotations[0,]
@@ -102,7 +106,7 @@ stacks = unique(stacks)
 stacks = stacks[order(stacks[,1], stacks[,4]),]
 
 # get a data.frame of all spans with graduation and the attitude spans that contain them
-graduation.all = stacks[0,]
+graduation = stacks[0,]
 look = 10     # how far ahead to look
 for(i in 1:length(stacks$span))
 {
@@ -113,14 +117,12 @@ for(i in 1:length(stacks$span))
     {
       if(stacks$charstart[i+n] <= stacks$charend[i]  # if the charstart for the 'next' is less than charend for this one
          & stacks$comment[i+n] == stacks$comment[i]  # and the two are in the same comment
-         & !(stacks$span[i] %in% punctuation)        # and this span is not just punctuation
-         & !(stacks$span[i+n] %in% punctuation)      # and neither is the 'next' one
          & (stacks$gralab[i] != "None" | stacks$grapol[i] != "None")  # and there's a graduation annotation in this row
          & (stacks$attlab[i+n] != "None" | stacks$attpol[i+n] != "None") # and an attitude annotation in the 'next' one
          )
       {
-        graduation.all = rbind(stacks[i,], graduation.all)  # then add this row to the df (graduation annotation)
-        graduation.all = rbind(stacks[i+n,], graduation.all) # and the 'next' one (attitude annotation)
+        graduation = rbind(stacks[i,], graduation)  # then add this row to the df (graduation annotation)
+        graduation = rbind(stacks[i+n,], graduation) # and the 'next' one (attitude annotation)
       }
     }
     # look behind for the same thing
@@ -128,18 +130,101 @@ for(i in 1:length(stacks$span))
     {
       if(stacks$charstart[i] <= stacks$charend[i-n]  # if the charstart for this one is greater than charend for the 'previous' one
          & stacks$comment[i-n] == stacks$comment[i]  # and the two are in the same comment
-         & !(stacks$span[i] %in% punctuation)        # and this span is not just punctuation
-         & !(stacks$span[i-n] %in% punctuation)      # and neither is the 'next' one
          & (stacks$gralab[i] != "None" | stacks$grapol[i] != "None")  # and there's a graduation annotation in this row
          & (stacks$attlab[i-n] != "None" | stacks$attpol[i-n] != "None") # and an attitude annotation in the 'previous' one
       )
       {
-        graduation.all = rbind(stacks[i,], graduation.all)  # then add this row to the df (graduation annotation)
-        graduation.all = rbind(stacks[i-n,], graduation.all) # and the 'previous' one (attitude annotation)
+        graduation = rbind(stacks[i,], graduation)  # then add this row to the df (graduation annotation)
+        graduation = rbind(stacks[i-n,], graduation) # and the 'previous' one (attitude annotation)
       }
     }
   }
 }
 
-graduation.all = unique(graduation.all)
-graduation.all = graduation.all[order(graduation.all[,1], graduation.all[,4]),]
+graduation = unique(graduation)
+graduation = graduation[order(graduation[,1], graduation[,4]),]
+graduation.oldnames = graduation # this df's rownames will be indices for the annotations df
+rownames(graduation) = 1:length(graduation$span)
+
+# add column for scope of graduation
+look = 10
+# find all rows with graduation (should be the same as focusrows + forcerows)
+candidates = graduation[graduation$grapol != "None" | graduation$gralab != "None",]
+# make a key so we know the original index of each row
+for (i in 1:length(candidates$span))
+{
+  index.key[i] = strtoi(rownames(candidates[i,]))
+}
+# e.g. index.key[1] returns "15," meaning 15 is the original index of candidates[1,]
+graduation$scope_of = 0
+for(i in 1:length(candidates$span))
+{
+  graduation$scope_of[index.key[i]] = 0
+  for(n in 1:look)
+  {
+    if(match(index.key[i]+n, 1:length(graduation$span), nomatch=FALSE))
+    {
+      if(graduation$charstart[index.key[i]+n] <= graduation$charend[index.key[i]] 
+        & graduation$comment[index.key[i]+n] == graduation$comment[index.key[i]]) # same thing as before checking for overlap
+      {
+        graduation$scope_of[index.key[i]+n] = index.key[i]
+      }
+    }
+    if(match(index.key[i]-n, 1:length(graduation$span), nomatch=FALSE))
+    {
+      if (graduation$charstart[index.key[i]] <= graduation$charend[index.key[i]-n] # but looking backwards too
+          & graduation$comment[index.key[i]-n] == graduation$comment[index.key[i]])
+      {
+        graduation$scope_of[index.key[i]-n] = index.key[i]
+      }
+    }
+  }
+}
+
+graduation$belongs_to = 0
+for(i in 1:length(candidates$span))
+{
+  graduation$belongs_to[index.key[i]] = 0
+  for(n in 1:look)
+  {
+    if(match(index.key[i]-n, 1:length(graduation$span), nomatch=FALSE))
+    {
+      if (graduation$charstart[index.key[i]] <= graduation$charend[index.key[i]-n] # looking backwards only
+          & graduation$comment[index.key[i]-n] == graduation$comment[index.key[i]])
+      {
+        graduation$belongs_to[index.key[i]] = index.key[i]-n
+      }
+    }
+  }
+}
+
+# find graduation spans with ambiguous scopes
+graduation.ambig = graduation[0,]
+look = 5
+for(i in 1:length(graduation$span))
+{
+  for(n in 1:look)
+  {
+    if (match(i+n, 1:length(graduation$span), nomatch=FALSE))
+    {
+      if(graduation$scope_of[i] != 0 # look for matching scopes, signaling ambiguity
+         & graduation$scope_of[i] == graduation$scope_of[i+n])
+      {
+        # add this row to graduation.ambig
+        graduation.ambig = rbind(graduation[i,], graduation.ambig)
+        # and the next one
+        graduation.ambig = rbind(graduation[i+n,], graduation.ambig)
+        # and the one of which this is the scope
+        graduation.ambig = rbind(graduation[graduation$scope_of[i],], graduation.ambig)
+      }
+    }
+  }
+}
+
+graduation.ambig = unique(graduation.ambig)
+# order the rows properly
+neworder = c()
+for (i in 1:length(rownames(graduation.ambig))) neworder[i] = strtoi(rownames(graduation.ambig)[i])
+graduation.ambig = graduation.ambig[order(neworder),]
+
+# this doesn't include cases where there's multiple graduation in one attitude, e.g. graduation[53:57,]
